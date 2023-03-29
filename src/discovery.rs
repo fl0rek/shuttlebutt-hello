608ea4421255d0c009c8bdf6a7373be3e2b7e68f
Mikołaj Florkiewicz <mikolaj@florkiewicz.me>
2023-03-29 16:03:38 +0200

    Fix possible issue with UDP message truncation

--- src/discovery.rs
+++ src/discovery.rs
@@ -1,10 +1,13 @@
 use crate::{PeerInfo, SetupError};
+use bytes::Bytes;
 use std::net::{SocketAddr, UdpSocket};
+
+const DISCOVERY_MESSAGE_BUFFER_LEN: usize = 512;
 
 /// listen on UDP port for local SSB servers and return _first_ match
 pub fn discover_local_peer(port: u16) -> Result<PeerInfo, SetupError> {
     // we're using socket2 here, so that we can set SO_REUSEADDR
-    // this is helpful in case where other ssb node also wants to listen for local discovery
+    // this is helpful in case where some other ssb node also wants to listen for local discovery
     let socket = socket2::Socket::new(socket2::Domain::IPV4, socket2::Type::DGRAM, None)?;
     let bind_address: SocketAddr = format!("0.0.0.0:{port}").parse().map_err(|e| {
         log::warn!("Cannot prepare addr to bind to for local discovery: {e}");
@@ -14,25 +17,31 @@ pub fn discover_local_peer(port: u16) -> Result<PeerInfo, SetupError> {
     socket.bind(&bind_address.into())?;
     let socket: UdpSocket = socket.into();
 
-    // TODO: 256 bytes should work in most situations, but we still don't handle truncated messages
-    // correctly
-    let mut read_buffer = [0; 256];
+    // while reading to a half-kb byte buffer should handle most real-world situations
+    // real world implementation shuld consider dynamically resising the window (with reasonable
+    // upper limit)
+    let mut read_buffer = [0; DISCOVERY_MESSAGE_BUFFER_LEN];
 
     loop {
         let (count, src) = socket
-            .recv_from(&mut read_buffer)
+            .recv_from(read_buffer.as_mut())
             .map_err(SetupError::IoError)?;
-
         log::trace!("received {count} bytes from {src}");
 
-        let discovery_buffer = String::from_utf8(read_buffer[0..count].to_vec()).map_err(|e| {
-            log::warn!("Failed to utf8 parse received SSB discovery message: {e}");
-            SetupError::ErrorParsingDiscoveryMessage
-        })?;
+        if count == DISCOVERY_MESSAGE_BUFFER_LEN {
+            log::warn!("received possibly truncated message")
+        }
 
-        log::info!("received discovery message: {discovery_buffer}");
+        // we split message with bytes crate, because truncating it may break utf8
+        // this way, only the last address will be affected
+        let discovery_bytes = Bytes::copy_from_slice(&read_buffer[0..count]);
+        for address_bytes in discovery_bytes.split(|b| *b == b';') {
+            let address = String::from_utf8(address_bytes.to_vec()).map_err(|e| {
+                log::warn!("Failed to utf8 parse received SSB address: {e}");
+                SetupError::ErrorParsingDiscoveryMessage
+            })?;
+            log::info!("received address from discovery message: {address}");
 
-        for address in discovery_buffer.split(';') {
             match address.parse() {
                 Ok(peer_info) => return Ok(peer_info),
                 Err(_) => {
